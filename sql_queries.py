b85926f61140b49f65066fb9f8038b401d206278
André Petersen <mail@andrepetersen.de>
2019-08-26 07:41:28 +0200

    Added primary keys and not null contrainsts. Also, defined handling of conflicts. PEP8 style guidelines.

--- sql_queries.py
+++ sql_queries.py
@@ -9,56 +9,131 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""
-	CREATE TABLE IF NOT EXISTS songplays (songplay_id int, start_time bigint, user_id int, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent text);
+	CREATE TABLE IF NOT EXISTS songplays (
+	songplay_id serial primary key, 
+	start_time bigint not null, 
+	user_id varchar not null, 
+	level varchar, 
+	song_id varchar, 
+	artist_id varchar, 
+	session_id int, 
+	location varchar, 
+	user_agent text
+	);
 """)
 
 user_table_create = ("""
-	CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar, last_name varchar, gender varchar, level varchar);
+	CREATE TABLE IF NOT EXISTS users (
+	user_id varchar not null primary key, 
+	first_name varchar not null, 
+	last_name varchar not null, 
+	gender varchar, 
+	level varchar
+	);
 """)
 
 song_table_create = ("""
-	CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, year int, duration numeric);
+	CREATE TABLE IF NOT EXISTS songs (
+	song_id varchar not null primary key, 
+	title varchar not null, 
+	artist_id varchar not null, 
+	year int, 
+	duration numeric
+	);
 """)
 
 artist_table_create = ("""
-	CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar, location varchar, latitude decimal, longitude decimal);
+	CREATE TABLE IF NOT EXISTS artists (
+	artist_id varchar not null primary key, 
+	name varchar not null, 
+	location varchar, 
+	latitude decimal, 
+	longitude decimal
+	);
 """)
 
 time_table_create = ("""
-	CREATE TABLE IF NOT EXISTS time (start_time bigint, hour smallint, day smallint, week smallint, month smallint, year smallint, weekday smallint);
+	CREATE TABLE IF NOT EXISTS time (
+	start_time bigint not null primary key, 
+	hour smallint not null, 
+	day smallint not null, 
+	week smallint not null, 
+	month smallint not null, 
+	year smallint not null, 
+	weekday smallint not null
+	);
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-	INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+	INSERT INTO songplays (
+		start_time, 
+		user_id, 
+		level, 
+		song_id, 
+		artist_id, 
+		session_id, 
+		location, 
+		user_agent
+	) 
 	VALUES
-	(%s, %s, %s, %s, %s, %s, %s, %s, %s)
+	(%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
-	INSERT INTO users (user_id, first_name, last_name, gender, level) 
+	INSERT INTO users (
+		user_id, 
+		first_name, 
+		last_name, 
+		gender, 
+		level
+	) 
 	VALUES
 	(%s, %s, %s, %s, %s)
+	ON CONFLICT(user_id) DO UPDATE SET level = EXCLUDED.level
 """)
 
 song_table_insert = ("""
-	INSERT INTO songs (song_id, title, artist_id, year, duration) 
+	INSERT INTO songs (
+		song_id, 
+		title, 
+		artist_id, 
+		year, 
+		duration
+	) 
 	VALUES
 	(%s, %s, %s, %s, %s)
+	ON CONFLICT(song_id) DO NOTHING
 """)
 
 artist_table_insert = ("""
-	INSERT INTO artists (artist_id, name, location, latitude, longitude)
+	INSERT INTO artists (
+		artist_id, 
+		name, 
+		location, 
+		latitude, 
+		longitude
+	)
 	VALUES
 	(%s, %s, %s, %s, %s)
+	ON CONFLICT(artist_id) DO NOTHING
 """)
 
 
 time_table_insert = ("""
-	INSERT INTO time (start_time, hour, day, week, month, year, weekday) 
+	INSERT INTO time (
+		start_time, 
+		hour, 
+		day, 
+		week, 
+		month, 
+		year, 
+		weekday
+	) 
 	VALUES
 	(%s, %s, %s, %s, %s, %s, %s)
+	ON CONFLICT(start_time) DO NOTHING
 """)
 
 # FIND SONGS
